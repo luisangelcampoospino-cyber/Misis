2f233a2c3e7575690fc264fc403e29ec0a0a2386
luisangelcampoospino-cyber <luisangelcampoospino@gmail.com>
2026-05-23 13:22:13 -0500

    Update Index.ts

--- Index.ts
+++ Index.ts
@@ -1,11 +1,1 @@
-import { Client, GatewayIntentBits } from "discord.js";
-
-const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
-});
-
-client.once("ready", () => {
-  console.log(`Conectado como ${client.user?.tag}`);
-});
-
-client.login("MTQ4MTM4OTU1ODY3ODY4NzgzOQ.G7dNuU.fsXqddtBu2FxnXgxyYTnpLSbwEOpFWqO8GXdUg");
+console.log("Bot iniciado");
