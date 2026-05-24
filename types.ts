1d9145a01f3712814d4e8567ed8c6cdf8fe4f99d
luisangelcampoospino-cyber <luisangelcampoospino@gmail.com>
2026-05-23 19:52:31 -0500

    Add interfaces for command and bot client types

--- types.ts
+++ types.ts
@@ -1,1 +1,53 @@
-export {};
+import {
+  ChatInputCommandInteraction,
+  Client,
+  Collection,
+  SlashCommandBuilder,
+  SlashCommandOptionsOnlyBuilder,
+  SlashCommandSubcommandsOnlyBuilder,
+} from "discord.js";
+
+export interface Command {
+  data:
+    | SlashCommandBuilder
+    | SlashCommandOptionsOnlyBuilder
+    | SlashCommandSubcommandsOnlyBuilder
+    | Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup">;
+  execute: (interaction: ChatInputCommandInteraction) => Promise<void>;
+}
+
+export interface BotClient extends Client {
+  commands: Collection<string, Command>;
+  xpCooldowns: Set<string>;
+}
+
+export interface EconomyData {
+  [userId: string]: {
+    balance: number;
+    lastDaily: number;
+    lastWork: number;
+  };
+}
+
+export interface WarnData {
+  [guildId: string]: {
+    [userId: string]: string[];
+  };
+}
+
+export interface LevelData {
+  [userId: string]: {
+    xp: number;
+    level: number;
+  };
+}
+
+export interface GuildConfig {
+  [guildId: string]: {
+    welcomeChannelId: string | null;
+    autoroleId: string | null;
+    automodEnabled: boolean;
+    antiSpamEnabled: boolean;
+  };
+}
+
